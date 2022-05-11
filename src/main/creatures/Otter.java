2d7fdd1945b57ebf9d4f6accccf5297ff2cf6791
redmondjd22 <redmondj22@gmail.com>
2022-05-11 08:19:02 -0400

    Otter class added

---
+++ src/main/creatures/Otter.java
@@ -0,0 +1,10 @@
+package creatures;
+
+public class Otter extends SeaCreature{
+    public void method2(){
+        System.out.println ("Paws");
+    }
+    public String toString(){
+        return "Ocean Dwelling";
+    }
+}
